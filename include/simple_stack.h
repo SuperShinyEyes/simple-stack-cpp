7a21bcd8af64bb4326cba4e193a826e477e346f8
Seyoung Park <seyoung.arts.park@protonmail.com>
2024-08-17 20:02:54 +0300

    Build C++ project structure

---
+++ include/simple_stack.h
@@ -0,0 +1,6 @@
+#ifndef SIMPLE_STACK_H
+#define SIMPLE_STACK_H
+
+int add(int a, int b);
+
+#endif // SIMPLE_STACK_H
---
+++ src/simple_stack.cpp
@@ -0,0 +1,15 @@
+#include "simple_stack.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+// template <class T>
+// class Stack {
+
+// public:
+//     Stack(int size)
+//     // Return and remove the top item
+//     T pop();
+//     push(T);
+// };
