c97862dc776e60f57facef0aab1f8f5307f77d54
Seyoung Park <seyoung.arts.park@protonmail.com>
2024-08-18 11:59:44 +0300

    Add StackList constructor

--- include/simple_stack.h
+++ include/simple_stack.h
@@ -3,4 +3,20 @@
 
 int add(int a, int b);
 
+template <class T>
+class StackList {
+    int allocated_size;
+    T *stack;
+public:
+    StackList(int size) {
+        allocated_size = size;
+    };
+    // Return and remove the top item
+    T pop();
+    void push(T);
+    int get_size() const {
+        return allocated_size;
+    }
+};
+
 #endif // SIMPLE_STACK_H
---
+++ tests/test_simple_stack.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "simple_stack.h"
+
+TEST(AdditionTest, HandlesPositiveInput) {
+    EXPECT_EQ(add(1, 2), 3);
+}
+
+TEST(AdditionTest, HandlesNegativeInput) {
+    EXPECT_EQ(add(-1, -1), -2);
+}
+
+TEST(StackListTest, HandlesConstructor) {
+    StackList<int> stack(10);
+    EXPECT_EQ(stack.get_size(), 10);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
